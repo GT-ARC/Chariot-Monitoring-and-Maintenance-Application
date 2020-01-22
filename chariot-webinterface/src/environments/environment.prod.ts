d4a8141ae59e74b735685418c76cd03e642ba1bd
Frederic <Frederic.Abraham1996@googlemail.com>
2020-01-22 11:23:00 +0100

    changed kms url

--- chariot-webinterface/src/environments/environment.prod.ts
+++ chariot-webinterface/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   databaseUrl: 'http://chariot-km.dai-lab.de:8080/v1/',
   monitoringServiceURL: 'http://chariot-km.dai-lab.de:8080/v1/monitoringservice/',
-  serviceUrl: 'http://chariot-km.dai-lab.de:8080/v1/services/?format=json',
+  serviceUrl: 'http://chariot-km.dai-lab.de:8080/v1/services?format=json',
   proxyAgentAddress: 'http://chariot-main.dai-lab.de:8080/chariot/sendAction'
 };
