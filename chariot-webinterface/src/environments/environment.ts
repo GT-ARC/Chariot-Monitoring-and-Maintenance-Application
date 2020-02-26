66f6c2e7267e43d1f7471bd646336b14920c8005
Frederic <Frederic.Abraham1996@googlemail.com>
2020-02-26 16:28:19 +0100

    changed version

--- chariot-webinterface/src/environments/environment.ts
+++ chariot-webinterface/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  version: "1.1",
+  version: "1.2",
   production: false,
   databaseUrl: 'http://chariot-km.dai-lab.de:8080/v1',
   monitoringServiceURL: 'http://chariot-km.dai-lab.de:8080/v1/monitoringservice/',
